268fece870448ba7d8661ddbeab42fd72dcd245c
Hien Phan <tpm.phanconghien@gmail.com>
2020-05-18 16:01:08 +0700

    group tests

--- cypress/integration/todomvc.spec.js
+++ cypress/integration/todomvc.spec.js
@@ -1,17 +1,25 @@
 /// <reference types="cypress" />
 
-it ('should be able to add new todo', () => {
-    cy.visit('http://todomvc-app-for-testing.surge.sh')
+describe('todo actions', () => {
+    beforeEach(() => {
+        cy.visit('http://todomvc-app-for-testing.surge.sh')
+        cy.get('.new-todo', {timeout: 6000}).type("Clean room{enter}")
+    })
 
-    cy.get('.new-todo', {timeout: 6000}).type("Clean room{enter}")
-
-    cy.get('label').should('have.text', 'Clean room')
-
-    cy.get('.toggle').should('not.be.checked', true)
-
-    cy.get('.toggle').click()
-
-    cy.get('.toggle').should('be.checked', true)
-    cy.get('label').should('have.css', 'text-decoration-line', 'line-through')
-    cy.contains('Clear completed').click()
+    it ('should be able to add new todo', () => {
+        cy.get('label').should('have.text', 'Clean room')
+        cy.get('.toggle').should('not.be.checked', true)
+    })
+    
+    it ('should mark a todo as completed', () => {
+        cy.get('.toggle').click()
+        cy.get('label').should('have.css', 'text-decoration-line', 'line-through')
+    
+    })
+    
+    it ('should clear completed todos', () => {
+        cy.get('.toggle').click()
+        cy.contains('Clear completed').click()
+        cy.get('.todo-list').should('not.have.descendants', 'li')
+    })
 })
