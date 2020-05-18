957b5f237ceb8bb2b248f4152747093d3038ea01
Hien Phan <tpm.phanconghien@gmail.com>
2020-05-18 15:44:45 +0700

    add validation

--- cypress/integration/todomvc.spec.js
+++ cypress/integration/todomvc.spec.js
@@ -1,11 +1,17 @@
 /// <reference types="cypress" />
 
-it ('should navgigate to the TodoMVC app', () => {
+it ('should be able to add new todo', () => {
     cy.visit('http://todomvc-app-for-testing.surge.sh')
 
     cy.get('.new-todo', {timeout: 6000}).type("Clean room{enter}")
 
+    cy.get('label').should('have.text', 'Clean room')
+
+    cy.get('.toggle').should('not.be.checked', true)
+
     cy.get('.toggle').click()
 
+    cy.get('.toggle').should('be.checked', true)
+    cy.get('label').should('have.css', 'text-decoration-line', 'line-through')
     cy.contains('Clear completed').click()
 })
